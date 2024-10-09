26a456b2c0fb833a4c179f6e96cb8480d11425ad
siddharth <siddharthtiwari1265@gmail.com>
2024-10-09 20:28:32 +0530

    somechanges

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import HeroImage from '../assets/dp.jpg';
+import HeroImage from '../assets/logo.avif';
 import { MdOutlineKeyboardArrowRight } from 'react-icons/md';
 import { Link } from 'react-scroll';
 
